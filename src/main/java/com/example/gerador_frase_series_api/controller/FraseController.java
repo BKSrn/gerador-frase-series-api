2374a5164f950bde54117f3ce2f05c9054419d20
Bernardo Kist Souza <bekist2006@gmail.com>
2025-10-29 16:51:45 -0300

    update FraseController.java

--- src/main/java/com/example/gerador_frase_series_api/controller/FraseController.java
+++ src/main/java/com/example/gerador_frase_series_api/controller/FraseController.java
@@ -5,14 +5,23 @@ import com.example.gerador_frase_series_api.model.Frase;
 import com.example.gerador_frase_series_api.service.FraseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/series")
 public class FraseController {
     @Autowired
     FraseService fraseService;
 
-    @GetMapping("/series/frases")
+    @GetMapping
+    public List<FraseDTO> buscarTodasFrases(){
+        return fraseService.buscarTodasFrases();
+    }
+
+    @GetMapping("/frases")
     public FraseDTO buscarFrase(){
         return fraseService.buscarFraseAleatoria();
     }
--- src/main/java/com/example/gerador_frase_series_api/service/FraseService.java
+++ src/main/java/com/example/gerador_frase_series_api/service/FraseService.java
@@ -6,6 +6,9 @@ import com.example.gerador_frase_series_api.repository.FraseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class FraseService {
     @Autowired
@@ -15,5 +18,13 @@ public class FraseService {
         Frase frase = fraseRepository.buscarFraseAleatoria();
 
         return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
+    }
+
+    public List<FraseDTO> buscarTodasFrases() {
+        List<Frase> frases = fraseRepository.findAll();
+
+        return frases.stream()
+                .map(f -> new FraseDTO(f.getTitulo(), f.getFrase(), f.getPersonagem(), f.getPoster()))
+                .collect(Collectors.toList());
     }
 }
